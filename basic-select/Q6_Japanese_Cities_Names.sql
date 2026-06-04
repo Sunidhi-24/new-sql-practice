17d035c23a8eb923eb0332ee56012591e78d73a7
Sunidhi-24 <149368488+Sunidhi-24@users.noreply.github.com>
2026-06-05 00:37:14 +0530

    Add SQL query for Japanese cities in CITY table

---
+++ basic-select/Q6_Japanese_Cities_Names.sql
@@ -0,0 +1,11 @@
+/*
+Problem: Japanese Cities' Names
+
+Task:
+Display the names of all cities in the CITY table
+where CountryCode = 'JPN'.
+*/
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
