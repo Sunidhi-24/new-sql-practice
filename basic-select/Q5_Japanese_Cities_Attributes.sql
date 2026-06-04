69effb3d7edaabceac4ba32c3694402f127fd015
Sunidhi-24 <149368488+Sunidhi-24@users.noreply.github.com>
2026-06-05 00:34:53 +0530

    Created SQL query for Japanese cities' attributes
    Add SQL query to select attributes of Japanese cities.

---
+++ basic-select/Q5_Japanese_Cities_Attributes.sql
@@ -0,0 +1,10 @@
+/*
+Problem: Japanese Cities' Attributes
+
+Task:
+Display all columns for every city in the CITY table where CountryCode = 'JPN'.
+*/
+
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
